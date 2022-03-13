f1da5c0aa02a33ad406e3b1da7844f75530fcb4b
Chris Ha <chunghha@users.noreply.github.com>
2022-03-12 21:00:32 -0600

    initial commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Nuxt3 + urql</h1>
+  <nav>
+    <nuxt-link to="/">index</nuxt-link> -
+    <nuxt-link to="/countries">countries</nuxt-link>
+  </nav>
+  <hr>
+  <nuxt-page />
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <section>
+    <p>Demo of nuxt3 SSR querying a graphql api</p>
+    <p>
+      <a href="https://github.com/bicouy0/nuxt3-urql" target="_blank" rel="noopener">forked from bicouy0/nuxt3-urql</a>
+    </p>
+  </section>
+</template>
